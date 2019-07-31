0c495f9953a74814742ad2d9a6795aea5fd2aa90
Maci Slenes <maci.slenes@gmail.com>
2019-07-30 23:21:04 -0700

    final project with readme and error proofed

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # gif-tastic
 
-This project will help you take an input display it as a button and hit an api to display gifs and their ratings
+This project will help you take an input of an animal, display it as a button with other animals and hit an api to display animal gifs and their ratings
 
 This will be useful to help keep you entertained with funny GIF's to watch
 
 You can get started by loading the page 
     Select any of the already generated buttons
     Let it generate 10 random GIFs
-If you want to select your own GIFs to diplsy type it in the text box and add it as a button
+If you want to select your own GIFs to display type it in the text box and add it as a button
     Click the button
     Watch the magic
 
-This was commented out and written it in simple english by Maci Slenes
-Thanks for your visiting
+This was commented out and written in simple english by Maci Slenes
+Thanks for visiting
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-// On document.ready you are going to load an array of strings called animals and it's going load the following animals
+// On document.ready you are going to create an array of strings called animals and it's going load the following animals
 $(document).ready(function() {
   // declaring our variables under animals
   var animals = [
@@ -15,16 +15,17 @@ $(document).ready(function() {
     // classToAdd is being used to store the name of the css and styling class
     // areaToAddTo is being used to store where we are going to display the animals array as buttons
   function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
-    // here we are going to empty the areaToAddTo Div so we don't just keep adding to the same space
+    // here we are going to empty the areaToAddTo Div so we don't just keep adding buttons to the same space with the existing buttons
     $(areaToAddTo).empty();
 
     // using a for loop we are going to loop over our array of animals
     for (var i = 0; i < arrayToUse.length; i++) {
-      // declaring a new variable and assinging it an html button
+      // declaring a new variable and dynaically creating a new html button
       var a = $("<button>");
-      // using .addClass() to assign the class we will pass through the function
+      // using .addClass() to assign the class that is passed to the function
       a.addClass(classToAdd);
-      // we are adding a .attr() assinging a "data-type" for the button and giving it the value of the animal in the array
+      // we are adding a .attr() assigning a "data-type" for the button and giving it the value of the animal in the array
+      // this will later be used by the ajax call to form the query to the giphy API
       a.attr("data-type", arrayToUse[i]);
       // adding .text() to the button 
       // assigns the text based on the name as it iterates through the loop
@@ -39,7 +40,7 @@ $(document).ready(function() {
     // taking the "#animals" section in the html and removing any existing content
     $("#animals").empty();
     // select all of the ".animal-button" and .removeClass the "active" attribute
-    // that way anything that was previously highlighted will no longer be
+    // that way anything that was previously highlighted as "active" will no longer be
     $(".animal-button").removeClass("active");
     // $()this represents the html element that triggered the event
     // in this case the even is a "click"
@@ -47,13 +48,13 @@ $(document).ready(function() {
     // giving it the highlighted box surrounding the selected button
     $(this).addClass("active");
     // declaring a new variable with the value of $(this) and assigning it the .attr of our "data-type"
-    // that way we can later use that type to search within gyphy for that specific elemet name
+    // that way we can later use that type to search within giphy for that specific elemet name
     var type = $(this).attr("data-type");
     // declaring a new variable and assining it the giphy url we want to hit with an ajax call
-    // building the url by feeding the parameter q with the type
-    // the type is the "data-type" of the button which was assined the animal name
+    // building the url by feeding 3 parameters q, api_key and limit
+    // q, is the type (the "data-type") of the button which was assigned the animal name
     // the api_key which is our api key 
-    // limit which we have set to 10 ==> telling the api to return 10 reults
+    // limit which we have set to 10 ==> telling the api to return 10 reults at most
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     // here is the call to our ajax
@@ -74,18 +75,18 @@ $(document).ready(function() {
         for (var i = 0; i < results.length; i++) {
           // dynamically creating a new div called "animal-item" and assining it to animalDiv
           var animalDiv = $("<div class=\"animal-item\">");
-          // declaring a new variable called rating and assinging it the value of the .rating return from within theresults[i] as it loop 
+          // declaring a new variable called rating and assigning it the value of the .rating return from within theresults[i] as it loop 
           var rating = results[i].rating;
-          // dynamically creating a new <p> tag and assinging the text as the rating of the gif
+          // dynamically creating a new <p> tag and assigning the text as the rating of the gif
           var p = $("<p>").text("Rating: " + rating);
 
-          // assinging a new animated variable as the .images.fixed.heigh.url from within the results returned from the api
+          // assigning a new animated variable as the .images.fixed.heigh.url from within the results returned from the api
           var animated = results[i].images.fixed_height.url;
-          // assinging a new still variable as the .images.fixed.heigh_still.url from within the results
+          // assigning a new still variable as the .images.fixed.heigh_still.url from within the results
           var still = results[i].images.fixed_height_still.url;
           // dynamically creating a new <img> element 
           var animalImage = $("<img>");
-          // assinging varirous data values we will need to show the still and moving images
+          // assigning varirous data values we will need to show the still and moving images
           //  .attr of "src" , "data-still", and "data-state" set to "still" 
           // still was declared as the still image from the api return
           animalImage.attr("src", still);
@@ -107,34 +108,34 @@ $(document).ready(function() {
       });
   });
 
-// here is our .on("click", function(){}) inside this we are listening for a click on the images with the ".animal-image" class that we dynamically created 
+// here is our .on("click", function(){}) inside this we are listening for a click on the images with the ".animal-image" class that we dynamically created . this function will pause a moving image or unpause a still image.
 $(document).on("click", ".animal-image", function() {
 
-  // declaring a new variable called state and assinging it the return of the "data-state".attr  of the item clicked using $(this) 
+  // declaring a new variable called state and assigning it the return of the "data-state".attr  of the item clicked using $(this) 
     var state = $(this).attr("data-state");
     // an if() statement saying if the return of the state is "still" run this code within the {}
     if (state === "still") {
-      // assinging a new .attr to the item clicked using $(this)
-      // we are assinging a new source as "data-animate" 
+      // assigning a new .attr to the item clicked using $(this)
+      // we are assigning a new source as "data-animate" 
       // "data-animate" was assined when we dynamicalled created the elements
-      // "data-animate" contains the api link to the animated version of the gif
+      // "data-animate" contains the url to the animated version of the gif
       $(this).attr("src", $(this).attr("data-animate"));
       // setting a new .attr to the "data-state" as "animate"
       $(this).attr("data-state", "animate");
     }
     // if the above statement is not true run this code
     else {
-      // assinging a new .attr to the item clicked using $(this)
-      // we are assinging a new source as "data-still" 
+      // assigning a new .attr to the item clicked using $(this)
+      // we are assigning a new source as "data-still" 
       // "data-still" was assined when we dynamicalled created the elements
-      // "data-still" contains the api link to the still version of the gif
+      // "data-still" contains the url to the still version of the gif
       $(this).attr("src", $(this).attr("data-still"));
       // setting a new .attr to the "data-state" as "still"
       $(this).attr("data-state", "still");
     }
   });
   
-// here is our .on("click", function(){}) inside this we are listening for a click on the images with the ".add-animal" class
+// here is our .on("click", function(){}) inside this we are listening for a click on the elements with the ".add-animal" class
   $("#add-animal").on("click", function(event) {
     event.preventDefault();
     // declaring a new variable based on it's .eq(0) position of the element in the jQueary object
@@ -151,7 +152,7 @@ $(document).on("click", ".animal-image", function() {
     populateButtons(animals, "animal-button", "#animal-buttons");
 
   });
-    // here is the call to the populateButtons() 
+    // here is the call to the populateButtons() when the page is first loaded
       // passing animals through the arrayToUse
       // "animal-button" as the classToAdd 
       // "#animal-buttons" id of the html element for areaToAddTo
